dd027aea71007ae6b265714b717e002b77f262b6
Giuseppe Scrivano <gscrivano@gnu.org>
2013-03-29 21:16:21 +0100

    Update copyright years.

--- src/tcal.c
+++ src/tcal.c
@@ -7,7 +7,7 @@
 *             Works *only* for Gregorian years!
 *
 *  Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
-*  2009, 2010, 2011 Free Software Foundation, Inc.
+*  2009, 2010, 2011, 2013 Free Software Foundation, Inc.
 *  Copyright (c) 1995, 96, 1997, 2000 Thomas Esken
 *
 *  This software doesn't claim completeness, correctness or usability.
